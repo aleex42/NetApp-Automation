2942d189e73700fb542b51133b614fa6bcf5fb3c
Alexander Krogloth <root@template.noris.de>
2018-03-20 21:31:49 +0100

    fixed snapmirror

--- schedule_snapmirror.pl
+++ schedule_snapmirror.pl
@@ -1,56 +1,110 @@
 #!/usr/bin/perl
 
-# --
-# schedule_snapmirror.pl - Reschedule all snapmirror to new cron schedules 
-# https://github.com/aleex42/NetApp-Automation
-# Copyright (C) 2014 Alexander Krogloth, E-Mail: git <at> krogloth.de
-# --
-# This software comes with ABSOLUTELY NO WARRANTY. For details, see
-# the enclosed file COPYING for license information (GPL). If you
-# did not receive this file, see http://www.gnu.org/licenses/gpl.txt.
-# --
-
 use strict;
 use warnings;
-use feature qw/switch/;
 
-# specify new crons, i.e. "daily_0:10, daily_6:10, ..."
-my $new_cron = "^daily_";
+use lib "/usr/lib/netapp-manageability-sdk/lib/perl/NetApp";
+use NaServer;
+use NaElement;
+use Data::Dumper;
 
-unless($ARGV[0]){
-    print "ERROR: no cluster specified\n";
-    print "Usage: ./schedule_snapmirror.pl CLUSTER\n";
-    die;
-} 
+my $hostname "filer1.example.com";
+my $username = "admin";
+my $password = "password";
 
-my $cluster = $ARGV[0];
+my $s = new NaServer($hostname, 1 , 21);
+$s->set_server_type('FILER');
+$s->set_transport_type('HTTPS');
+$s->set_port(443);
+$s->set_style('LOGIN');
+$s->set_admin_user($username, $password);
 
-my @matching_crons = `ssh admin\@$cluster "cron show -fields name" | grep "$new_cron"`;
-my %crons;
+my $api = new NaElement('snapmirror-get-iter');
 
-foreach my $cron (@matching_crons){
-        $cron =~ s/\s+\z//;
-        $crons{$cron} = "0";
+my $xi = new NaElement('desired-attributes');
+$api->child_add($xi);
+
+my $xi1 = new NaElement('snapmirror-info');
+$xi->child_add($xi1);
+
+$xi1->child_add_string('destination-volume','<destination-volume>');
+$xi1->child_add_string('destination-vserver','<destination-vserver>');
+$xi1->child_add_string('source-volume','<source-volume>');
+$xi1->child_add_string('source-vserver','<source-vserver>');
+
+my $tag_elem = NaElement->new("tag");
+$api->child_add($tag_elem);
+
+my $out = $s->invoke_elem($api);
+
+my $next = "";
+
+my @snapmirror;
+
+while(defined($next)){
+    unless($next eq ""){
+        $tag_elem->set_content($next);
+    }
+
+    $api->child_add_string("max-records", 100);
+    my $snap_output = $s->invoke_elem($api);
+
+    my $num_records = $snap_output->child_get_string("num-records");
+
+    if($num_records eq 0){
+        last;
+    }
+
+    my @snapmirrors = $snap_output->child_get("attributes-list")->children_get();
+
+    foreach my $snap (@snapmirrors){
+
+        push(@snapmirror, {
+            'destination-volume' => $snap->child_get_string('destination-volume'),
+            'destination-vserver' => $snap->child_get_string('destination-vserver'),
+            'source-vserver' => $snap->child_get_string('source-vserver'),
+            'source-volume' => $snap->child_get_string('source-volume'),
+        });
+
+    }
+
+    $next = $snap_output->child_get_string("next-tag");
+
 }
-my @vservers = `ssh admin\@$cluster "snapmirror show -fields schedule" | grep -vE '([0-9]* entries|^source-path|^-------)' | grep ^[a-z] | awk '{ print \$3 }'`;
 
-foreach my $sched (@vservers){
-	$sched =~ s/\s+\z//;
-	if($sched =~ m/^daily_/){
-		$crons{$sched}++;
-	}
-}
+my $count = 0;
+my $schedule;
 
-my @snap_destinations = `ssh admin\@$cluster "snapmirror show -fields destination-path" | grep -vE '([0-9]* entries|^source-path|^-------)' | grep ^[a-z] | awk '{ print \$2 }'`;
+foreach my $snapmirror (@snapmirror){
 
-foreach my $snap (@snap_destinations){
-	$snap =~ s/\s+\z//;
-	my $new_cron = show_emptiest_cron();
-	`ssh admin\@$cluster "snapmirror modify -destination-path $snap -schedule $new_cron"`;
-	$crons{$new_cron}++;
-}
+    my $source_volume = $snapmirror->{'source-volume'};
+    my $source_vserver = $snapmirror->{'source-vserver'};
+    my $destination_volume = $snapmirror->{'destination-volume'};
+    my $destination_vserver = $snapmirror->{'destination-vserver'};
 
-sub show_emptiest_cron {
-	my @cron_keys = sort { $crons{$a} <=> $crons{$b} } keys(%crons);
-	return $cron_keys[0];
+    my $modulo = $count % 4;
+
+    if($modulo == 0){
+        $schedule = "hourly.0";
+    } elsif($modulo == 1){
+        $schedule = "hourly.15";
+    } elsif($modulo == 2){
+        $schedule = "hourly.30";
+    } elsif($modulo == 3){
+        $schedule = "hourly.45";
+    }
+
+    my $modify_api = new NaElement('snapmirror-modify');
+    $modify_api->child_add_string('destination-volume',$destination_volume);
+    $modify_api->child_add_string('destination-vserver',$destination_vserver);
+    $modify_api->child_add_string('schedule',$schedule);
+    $modify_api->child_add_string('source-volume',$source_volume);
+    $modify_api->child_add_string('source-vserver',$source_vserver);
+
+    my $modify_output = $s->invoke_elem($modify_api);
+
+    print Dumper($modify_output);
+
+    $count++;
+
 }
